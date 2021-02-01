93eec871588f50b01461ff7116bcf0c58efd96c5
JansenLefever <jansenlefever@gmail.com>
2021-02-01 23:51:01 +0000

    revised loop challenge

--- loop.sh
+++ loop.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-
+ 
 # Script:   201d2 Loops Challenge                    
 # Author:   Jansen Lefever                    
 # Date of latest revision:  1/29/21    
 # Purpose:   Loops                   
-
+ 
 # Basic for loop
-
+ 
 #variables
-psaux="ps aux"
+option="y"
 
-# loop
-for process in $psaux
+while [ $option == "y" ]
 do
-  $psaux
-  read -p "what process would you like to kill? Enter PID: " proc
-  process=$(pidof $proc)
-  kill $process
-  break
+  ps aux
+  echo "what process would you like to kill? Enter PID: "
+  read process
+  kill -9 $process
+  echo "Is there another process you would like to kill? (y/n)"
+  read option
 done
 
-echo done
 
-# End
+echo done
