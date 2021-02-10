7916c9c4651955c31dd472665e4e77e5f27e3559
JansenLefever <jansenlefever@gmail.com>
2021-02-09 19:59:04 -0800

    Grabs IPv4 Address

--- IP_AD.ps1
+++ IP_AD.ps1
@@ -6,15 +6,12 @@
 #Variable 
 $target = ipconfig /all
 
+#main
 $target > C:\Users\Driver8\Desktop\network_report.txt
     
 Out-File .\network_report.txt
     
-Select-String -Path "C:\Users\Driver8\Desktop\network_report.txt" -Pattern "Address:"
+Select-String -Path "C:\Users\Driver8\Desktop\network_report.txt" -Pattern "IPv4 Address"
     
 rm C:\Users\Driver8\Desktop\network_report.txt
 
-#So I have no idea why but I can't seem to get Select-String to work.
-#I've tried a bunch of different comabtions of things with little sucess.
-#Including putting the path in front followed by a |.
-#I've been at this for over an hour. Everyone else had signed off. I decided to take the L on this one.
