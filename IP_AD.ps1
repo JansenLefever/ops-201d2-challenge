223a94b180301998d31ebcc17ee726d74fa1fe74
JansenLefever <jansenlefever@gmail.com>
2021-02-10 18:39:08 -0800

    updated

--- IP_AD.ps1
+++ IP_AD.ps1
@@ -3,15 +3,15 @@
 # Date of last revision: 2/9/21
 # Purpose: Pulling up IP Address
 
-#Variable 
-$target = ipconfig /all
+Function Get-IpConfig{
+    $Target = ipconfig /all #Variable 
+    $Target
+}
 
 #main
-$target > C:\Users\Driver8\Desktop\network_report.txt
-    
-Out-File .\network_report.txt
+
+Get-IpConfig | Out-File -FilePath C:\Users\Driver8\Desktop\network_report.txt
     
 Select-String -Path "C:\Users\Driver8\Desktop\network_report.txt" -Pattern "IPv4 Address"
     
 rm C:\Users\Driver8\Desktop\network_report.txt
-
